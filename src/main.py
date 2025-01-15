c290e43ecc027210980ff53164b10f4ca3540f52
samuele <s.chimera@innonation.it>
2025-01-15 16:51:29 +0100

    DB list

--- src/main.py
+++ src/main.py
@@ -15,28 +15,61 @@ def main(context):
         .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
         .set_key("standard_9094aed773b56d8af3557aae5bf3dff3741bb11d78152ce195242c437ea0b35dab28a474fe341d38bbe80dd92ac48d0172e7a3135774dbba22cf50bd270162f3b6ad4f4cd79fe25637c283e5c55b3da2b28866e53f25adca5454a02de4f9964b3d9abce2a18c765898671b9c0ad962745f4740cc0fd32984e4492b7020fad8df")
     )
-    users = Users(client)
     databases = Databases(client)
+
+    # ID del database
+    database_id = "6729f7e30033d3541307"
+
     try:
+        if context.req.path == "/":
+            # Recupera tutti i documenti dalla collection
+            body = context.req.json if context.req.json else {}
+            collection_id = body.get("collection_id")
+            if not collection_id:
+                return context.res.json(
+                    {"error": "collection_id è obbligatorio."},
+                    status=400,
+                )
 
-        response = databases.get(database_id="6729f7e30033d3541307")
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log(response)
+            documents = databases.list_documents(database_id=database_id, collection_id=collection_id)
+            context.log("Documenti recuperati: " + str(documents))
+            return context.res.json({"documents": documents})
+
+        elif context.req.path.startswith("/update/"):
+            # Estrae l'ID della collection dall'URL
+            collection_id = context.req.path.split("/update/")[1]
+            if not collection_id:
+                return context.res.json(
+                    {"error": "collection_id non trovato nell'URL."},
+                    status=400,
+                )
+
+            # Estrae i dati dal corpo della richiesta
+            body = context.req.json if context.req.json else {}
+            document_id = body.get("document_id")
+            field_name = body.get("field_name")
+            new_value = body.get("new_value")
+
+            if not document_id or not field_name or new_value is None:
+                return context.res.json(
+                    {"error": "document_id, field_name e new_value sono obbligatori."},
+                    status=400,
+                )
+
+            # Aggiorna il documento nella collection
+            updated_data = {field_name: new_value}
+            response = databases.update_document(
+                database_id=database_id,
+                collection_id=collection_id,
+                document_id=document_id,
+                data=updated_data
+            )
+            context.log("Documento aggiornato: " + str(response))
+            return context.res.json({"message": "Documento aggiornato con successo!", "response": response})
+
+        else:
+            return context.res.json({"message": "Endpoint non valido."}, status=404)
+
     except AppwriteException as err:
-        context.error("Could not list databases: " + repr(err))
-
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+        context.error("Errore: " + repr(err))
+        return context.res.json({"error": repr(err)}, status=500)
