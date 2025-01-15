01127491e2cddf4dea06c1cf6f06afd80739365f
samuele <s.chimera@innonation.it>
2025-01-15 16:21:20 +0100

    DB list

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ def main(context):
         Client()
         .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
         .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
+        .set_key("standard_9094aed773b56d8af3557aae5bf3dff3741bb11d78152ce195242c437ea0b35dab28a474fe341d38bbe80dd92ac48d0172e7a3135774dbba22cf50bd270162f3b6ad4f4cd79fe25637c283e5c55b3da2b28866e53f25adca5454a02de4f9964b3d9abce2a18c765898671b9c0ad962745f4740cc0fd32984e4492b7020fad8df")
     )
     users = Users(client)
     databases = Databases(client)
